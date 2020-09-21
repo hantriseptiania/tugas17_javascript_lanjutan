bacab6f59e456b95a000fbef66e5fd4dfde635df
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-21 10:54:41 +0700

    Add files via upload

---
+++ tugas17.js
@@ -0,0 +1,12 @@
+var kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+
+console.log(kalimat1.toLowerCase())
+console.log(kalimat1.toUpperCase())
+console.log(kalimat2.toLowerCase())
+console.log(kalimat2.toUpperCase())
+
+
+var kalimat = "Lorem "
+
+console.log(kalimat.charCodeAt(0), "-", kalimat.charCodeAt(1), "-", kalimat.charCodeAt(2), "-", kalimat.charCodeAt(3), "-", kalimat.charCodeAt(4), "-", kalimat.charCodeAt(5))
